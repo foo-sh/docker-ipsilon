6efe3a49639e67e6831332df7564ae931c61d8ea
Timo Makinen <tmakinen@foo.sh>
2024-12-07 19:24:14 +0000

    Disable SAML2 only use OpenID

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,7 @@ RUN set -eux ; \
         ipsilon \
         ipsilon-authgssapi \
         ipsilon-authldap \
-        ipsilon-saml2 \
+        ipsilon-openid \
         python3-mysqlclient \
     ; \
     dnf -y clean all ; \
--- entrypoint.sh
+++ entrypoint.sh
@@ -30,8 +30,6 @@ ipsilon-server-install \
     --info-ldap-user-dn-template="uid=%(username)s,ou=People,${LDAP_BASEDN}" \
     --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
     --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
-    --saml2=yes \
-    --samlsessions-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_SESSIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
     --openid=yes \
     --openid-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENID}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
 
