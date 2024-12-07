2030ca639aff07f5fc577e2b443524949f9d1259
Timo Makinen <tmakinen@foo.sh>
2024-12-07 09:04:37 +0000

    Verify correct permissions for TLS key

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ fi
 [ "${IPSILON_DB_TRANSACTIONS:-}" = "" ] && IPSILON_DB_TRANSACTIONS="ipsilon"
 [ "${IPSILON_DB_SESSIONS:-}" = "" ] && IPSILON_DB_SESSIONS="ipsilon"
 
+instaal -m 0640 -user root -group ipsilon "$IPSILON_DB_KEY" "/etc/ssl/private/ipsilon.key"
+
 ipsilon-server-install \
     --root-instance \
     --hostname="idp.foo.sh" \
@@ -25,9 +27,9 @@ ipsilon-server-install \
     --info-ldap=yes \
     --info-ldap-server-url="${LDAP_URI}" \
     --info-ldap-user-dn-template="uid=%(username)s,ou=People,${LDAP_BASEDN}" \
-    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=${IPSILON_DB_KEY}&ssl_cert=${IPSILON_DB_CERT}" \
-    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=${IPSILON_DB_KEY}&ssl_cert=${IPSILON_DB_CERT}" \
-    --samlsessions-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_SESSIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=${IPSILON_DB_KEY}&ssl_cert=${IPSILON_DB_CERT}"
+    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --samlsessions-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_SESSIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
 
 # enable proxy support manually
 {
