831b95c36bfd035fa80221b2d991fd1828c74303
Timo Makinen <tmakinen@foo.sh>
2024-12-08 10:33:34 +0000

    Handle TLS options for DB connections better

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,38 @@ if [ -z "${LDAP_URI:-}" ]; then
 fi
 
 [ "${IPSILON_DB_USER:-}" = "" ] && IPSILON_DB_USER="ipsilon"
-[ "${IPSILON_DB_CA:-}" = "" ] && IPSILON_DB_CA="/etc/ssl/certs/ca-bundle.crt"
 
-install -m 0640 --owner root --group ipsilon "$IPSILON_DB_KEY" "/etc/ssl/private/ipsilon.key"
+TLSOPTS=""
+if [ -n "${IPSILON_DB_CA:-}" ]; then
+    if [ ! -r "$IPSILON_DB_CA" ]; then
+        echo "ERROR: Cannot read CA certificate '${IPSILON_DB_CA}'" 1>&2
+        exit 1
+    fi
+    TLSOPTS="&ssl=true&ssl_ca=${IPSILON_DB_CA}"
+fi
+
+if [ -n "${IPSILON_DB_KEY:-}" ] && [ -n "${IPSILON_DB_CERT:-}" ]; then
+    if ! openssl rsa -check -noout -in "$IPSILON_DB_KEY" > /dev/null 2>&1; then
+        echo "ERROR: Failed to read client private key '${IPSILON_DB_KEY}'" 1>&2
+        exit 1
+    fi
+    if ! openssl x509 -in "$IPSILON_DB_CERT" -noout > /dev/null 2>&1; then
+        echo "ERROR: Failed to read client certificate '${IPSILON_DB_CERT}'" 1>&2
+        exit 1
+    fi
+    install -m 0640 --owner root --group ipsilon "$IPSILON_DB_KEY" "/etc/ssl/private/ipsilon.key"
+    TLSOPTS="${TLSOPTS}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
+elif [ -n "${IPSILON_DB_KEY:-}" ]; then
+    echo "ERROR: Client private key configured but no certificate" 1>&2
+    exit 1
+elif [ -n "${IPSILON_DB_CERT:-}" ]; then
+    echo "ERROR: Client certificate configured but no private key" 1>&2
+    exit 1
+fi
+
+if [ -n "$TLSOPTS" ]; then
+    TLSOPTS="$(echo "$TLSOPTS" | cut -c 2-)"
+fi
 
 ipsilon-server-install \
     --root-instance \
@@ -24,11 +53,11 @@ ipsilon-server-install \
     --info-ldap=yes \
     --info-ldap-server-url="${LDAP_URI}" \
     --info-ldap-user-dn-template="uid=%(username)s,ou=People,${LDAP_BASEDN}" \
-    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
-    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS:-ipsilon}?${TLSOPTS}" \
+    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS:-ipsilon}?${TLSOPTS}" \
     --openidc=yes \
-    --openidc-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
-    --openidc-static-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC_STATIC:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
+    --openidc-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC:-ipsilon}?${TLSOPTS}" \
+    --openidc-static-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC_STATIC:-ipsilon}?${TLSOPTS}"
 
 # enable proxy support manually
 {
