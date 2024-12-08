86801bb24b9546186a28ae2ec0c05f0db5b921fc
Timo Makinen <tmakinen@foo.sh>
2024-12-08 09:52:47 +0000

    More cleaner way to set default db names

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,11 +10,6 @@ fi
 
 [ "${IPSILON_DB_USER:-}" = "" ] && IPSILON_DB_USER="ipsilon"
 [ "${IPSILON_DB_CA:-}" = "" ] && IPSILON_DB_CA="/etc/ssl/certs/ca-bundle.crt"
-[ "${IPSILON_DB_USERPREFS:-}" = "" ] && IPSILON_DB_USERPREFS="ipsilon"
-[ "${IPSILON_DB_TRANSACTIONS:-}" = "" ] && IPSILON_DB_TRANSACTIONS="ipsilon"
-[ "${IPSILON_DB_SESSIONS:-}" = "" ] && IPSILON_DB_SESSIONS="ipsilon"
-[ "${IPSILON_DB_OPENIDC:-}" = "" ] && IPSILON_DB_OPENIDC="ipsilon"
-[ "${IPSILON_DB_OPENIDC_STATIC:-}" = "" ] && IPSILON_DB_OPENIDC_STATIC="ipsilon"
 
 install -m 0640 --owner root --group ipsilon "$IPSILON_DB_KEY" "/etc/ssl/private/ipsilon.key"
 
@@ -29,11 +24,11 @@ ipsilon-server-install \
     --info-ldap=yes \
     --info-ldap-server-url="${LDAP_URI}" \
     --info-ldap-user-dn-template="uid=%(username)s,ou=People,${LDAP_BASEDN}" \
-    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
-    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --users-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_USERPREFS:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --transaction-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_TRANSACTIONS:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
     --openidc=yes \
-    --openidc-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
-    --openidc-static-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC_STATIC}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
+    --openidc-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}" \
+    --openidc-static-dburi="mysql://${IPSILON_DB_USER}:${IPSILON_DB_PASS}@${IPSILON_DB_HOST}/${IPSILON_DB_OPENIDC_STATIC:-ipsilon}?ssl=true&ssl_ca=${IPSILON_DB_CA}&ssl_key=/etc/ssl/private/ipsilon.key&ssl_cert=${IPSILON_DB_CERT}"
 
 # enable proxy support manually
 {
